bf672bf22a5ba4f94c46d2008985d683ec136d99
trevor <tmbarker9@gmail.com>
2022-12-08 19:05:11 -0800

    Add day 9 template

---
+++ Problems/Y2022/D09/Solution.cs
@@ -0,0 +1,18 @@
+using Problems.Y2022.Common;
+
+namespace Problems.Y2022.D09;
+
+public class Solution : SolutionBase2022
+{
+    public override int Day => 9;
+    
+    public override string Run(int part)
+    {
+        AssertInputExists();
+
+        return part switch
+        {
+            _ => ProblemNotSolvedString,
+        };
+    }
+}
