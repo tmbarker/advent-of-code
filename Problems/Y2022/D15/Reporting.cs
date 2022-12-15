c674a2e7d80235f573dd82cfb47b5eb9625c89de
trevor <tmbarker9@gmail.com>
2022-12-15 11:17:34 -0800

    Add 2022-15 part 2

---
+++ Problems/Y2022/D15/Reporting.cs
@@ -0,0 +1,15 @@
+using Utilities.DataStructures.Grid;
+
+namespace Problems.Y2022.D15;
+
+public readonly struct Reporting
+{
+    public Reporting(Vector2D sensorPos, Vector2D beaconPos)
+    {
+        SensorPos = sensorPos;
+        BeaconPos = beaconPos;
+    }
+    
+    public Vector2D SensorPos { get; }
+    public Vector2D BeaconPos { get; }
+}
--- Problems/Y2022/D15/Solution.cs
+++ Problems/Y2022/D15/Solution.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using Problems.Common;
 using Problems.Y2022.Common;
 using Utilities.DataStructures.Grid;
 using Utilities.Extensions;
@@ -12,6 +13,8 @@ public class Solution : SolutionBase2022
 {
     private const string SensorRegex = @".*x=(-?\d+).*y=(-?\d+).*x=(-?\d+).*y=(-?\d+)";
     private const int Row = 2000000;
+    private const int SearchAreaDimension = 4000000;
+    private const long TuningFrequencyMultiplier = SearchAreaDimension;
     
     public override int Day => 15;
     
@@ -20,62 +23,143 @@ public class Solution : SolutionBase2022
         var reportings = ParseReportings(GetInput());
         return part switch
         {
-            0 => CountBeaconExcludedPositions(reportings, Row).ToString(),
+            0 => CountBeaconExcludedPositions(reportings).ToString(),
+            1 => CalculateTuningFrequency(FindDistressBeacon(reportings)).ToString(),
             _ => ProblemNotSolvedString,
         };
     }
 
-    private static int CountBeaconExcludedPositions(IEnumerable<(Vector2D sensorPos, Vector2D beaconPos)> reportings, int row)
+    private static int CountBeaconExcludedPositions(IList<Reporting> reportings)
     {
-        var enumeratedReportings = reportings.ToList();
-        var excludedBeaconPositions = new HashSet<Vector2D>();
-        var sensorPositions = new HashSet<Vector2D>();
-        var beaconPositions = new HashSet<Vector2D>();
-
-        foreach (var (s, b) in enumeratedReportings)
+        var beaconExcludedPositions = new HashSet<Vector2D>();
+        var occupiedPositions = new HashSet<Vector2D>();
+        
+        foreach (var reporting in reportings)
         {
-            sensorPositions.EnsureContains(s);
-            beaconPositions.EnsureContains(b);
+            occupiedPositions.EnsureContains(reporting.SensorPos);
+            occupiedPositions.EnsureContains(reporting.BeaconPos);
         }
-
-        foreach (var (sensorPos, beaconPos) in enumeratedReportings)
+        
+        foreach (var reporting in reportings)
         {
+            var beaconPos = reporting.BeaconPos;
+            var sensorPos = reporting.SensorPos;
             var sensorRange = sensorPos.TaxicabDistance(beaconPos);
-            if (Math.Abs(sensorPos.Y - row) > sensorRange)
+            
+            if (Math.Abs(sensorPos.Y - Row) > sensorRange)
             {
                 continue;
             }
 
-            var dy = Math.Abs(sensorPos.Y - row);
-            var dx = sensorRange - dy;
-            
+            var dy = Math.Abs(sensorPos.Y - Row);
+            var maxDxInRange = sensorRange - dy;
+
             // Only sweep the minimum number of positions
-            var sweepStart = new Vector2D(sensorPos.X - dx - 1, row);
-            var sweepEnd = new Vector2D(sensorPos.X + dx + 1, row);
-            
+            var sweepStart = new Vector2D(sensorPos.X - maxDxInRange, Row);
+            var sweepEnd = new Vector2D(sensorPos.X + maxDxInRange, Row);
             var sweepPos = sweepStart;
-            var delta = Vector2D.Normalize(sweepEnd - sweepStart);
 
             while (sweepPos.X <= sweepEnd.X)
             {
-                var distanceFromSensor = sensorPos.TaxicabDistance(sweepPos);
-                if (distanceFromSensor <= sensorRange && !sensorPositions.Contains(sweepPos) && !beaconPositions.Contains(sweepPos))
+                if (!occupiedPositions.Contains(sweepPos))
                 {
-                    excludedBeaconPositions.EnsureContains(sweepPos);
+                    beaconExcludedPositions.Add(sweepPos);
                 }
-                sweepPos += delta;
+                sweepPos += Vector2D.Right;
             }
         }
         
-        return excludedBeaconPositions.Count;
+        return beaconExcludedPositions.Count;
     }
 
-    private static IEnumerable<(Vector2D sensorPos, Vector2D beaconPos)> ParseReportings(IEnumerable<string> lines)
+    private static long CalculateTuningFrequency(Vector2D beaconPos)
     {
-        return lines.Select(ParseReporting);
+        return beaconPos.X * TuningFrequencyMultiplier + beaconPos.Y;
+    }
+    
+    private Vector2D FindDistressBeacon(IEnumerable<Reporting> reportings)
+    {
+        var sensorRangeTuples = reportings.Select(r => 
+        {
+            var sensorPos = r.SensorPos;
+            var range = sensorPos.TaxicabDistance(r.BeaconPos);
+            return (sensorPos, range);
+        }).ToList();
+        
+        for (var i = 0; i < sensorRangeTuples.Count; i++)
+        {
+            var sensorPos = sensorRangeTuples[i].sensorPos;
+            var sensorRange = sensorRangeTuples[i].range;
+            var outOfRangePerimeterPositions = GetPerimeterPositionsInSearchArea(sensorPos, sensorRange);
+            
+            foreach (var searchPos in outOfRangePerimeterPositions)
+            {
+                var searchPosInRangeOfSensor = false;
+                for (var j = 0; j < sensorRangeTuples.Count; j++)
+                {
+                    if (sensorRangeTuples[j].sensorPos.TaxicabDistance(searchPos) > sensorRangeTuples[j].range)
+                    {
+                        continue;
+                    }
+                    
+                    searchPosInRangeOfSensor = true;
+                    break;
+                }
+
+                if (!searchPosInRangeOfSensor)
+                {
+                    return searchPos;
+                }
+            }
+        }
+
+        throw new NoSolutionException();
     }
 
-    private static (Vector2D sensorPos, Vector2D beaconPos) ParseReporting(string reporting)
+    private static HashSet<Vector2D> GetPerimeterPositionsInSearchArea(Vector2D sensorPos, int range)
+    {
+        var positionSet = new HashSet<Vector2D>();
+        var distance = range + 1;
+        
+        var vertices = new List<Vector2D>
+        {
+            sensorPos + distance * Vector2D.Up,
+            sensorPos + distance * Vector2D.Right,
+            sensorPos + distance * Vector2D.Down,
+            sensorPos + distance * Vector2D.Left,
+        };
+
+        for (var i = 0; i < vertices.Count - 1; i++)
+        {
+            var fromVertex = vertices[i];
+            var toVertex = vertices[i + 1];
+            var step = Vector2D.Normalize(toVertex - fromVertex);
+
+            var current = fromVertex;
+            while (current != toVertex + step)
+            {
+                if (IsPositionInSearchArea(current))
+                {
+                    positionSet.EnsureContains(current);   
+                }
+                current += step;
+            }
+        }
+
+        return positionSet;
+    }
+
+    private static bool IsPositionInSearchArea(Vector2D pos)
+    {
+        return pos is { X: >= 0, Y: >= 0 } and { X: <= SearchAreaDimension, Y: <= SearchAreaDimension };
+    }
+
+    private static IList<Reporting> ParseReportings(IEnumerable<string> lines)
+    {
+        return lines.Select(ParseReporting).ToList();
+    }
+
+    private static Reporting ParseReporting(string reporting)
     {
         var matches = Regex.Match(reporting, SensorRegex);
         
@@ -84,6 +168,6 @@ public class Solution : SolutionBase2022
         var beaconX = int.Parse(matches.Groups[3].Value);
         var beaconY = int.Parse(matches.Groups[4].Value);
 
-        return (new Vector2D(sensorX, sensorY), new Vector2D(beaconX, beaconY));
+        return new Reporting(new Vector2D(sensorX, sensorY), new Vector2D(beaconX, beaconY));
     }
 }
