0da22bc27da3d4f699fc704631e34d07dda04e59
tmbarker <tmbarker9@gmail.com>
2022-12-27 12:42:46 -0800

    Add 2021-14 solution

---
+++ Problems/Y2021/D14/Solution.cs
@@ -0,0 +1,122 @@
+﻿using System.Text.RegularExpressions;
+using Problems.Y2021.Common;
+using Utilities.Extensions;
+
+namespace Problems.Y2021.D14;
+
+/// <summary>
+/// Extended Polymerization: https://adventofcode.com/2021/day/14/input
+/// </summary>
+public class Solution : SolutionBase2021
+{
+    private const string RuleRegex = @"(.)(.) -> (.)";
+    private const int Steps1 = 10;
+    private const int Steps2 = 40;
+    
+    public override int Day => 14;
+    
+    public override object Run(int part)
+    {
+        Parse(GetInput(), out var polymer, out var rules);
+        return part switch
+        {
+            0 => GetMaxExtendedVariance(polymer, rules, Steps1),
+            1 => GetMaxExtendedVariance(polymer, rules, Steps2),
+            _ => ProblemNotSolvedString,
+        };
+    }
+
+    private static long GetMaxExtendedVariance(IList<char> polymer, IList<Rule> rules, int steps)
+    {
+        var extendedPolymer = ExtendPolymer(polymer, rules, steps);
+        var maxVariance = ComputeMaxFrequencyVariance(extendedPolymer, polymer.First(), polymer.Last());
+
+        return maxVariance;
+    }
+    
+    private static long ComputeMaxFrequencyVariance(Dictionary<(char, char), long> pairCounts, char pStart, char pEnd)
+    {
+        var doubledFrequencies = new Dictionary<char, long>();
+        foreach (var ((lhs, rhs), count) in pairCounts)
+        {
+            doubledFrequencies.EnsureContainsKey(lhs);
+            doubledFrequencies.EnsureContainsKey(rhs);
+            doubledFrequencies[lhs] += count;
+            doubledFrequencies[rhs] += count;
+        }
+
+        // The only polymer elements which aren't doubled are the LHS of the first pair, and the RHS of the last pair
+        doubledFrequencies[pStart]++;
+        doubledFrequencies[pEnd]++;
+
+        var sortedFrequencies = doubledFrequencies.Values
+            .Where(c => c > 0)
+            .Select(n => n/2)
+            .OrderBy(n => n)
+            .ToList();
+
+        return sortedFrequencies.Last() - sortedFrequencies.First();
+    }
+
+    private static Dictionary<(char, char), long> ExtendPolymer(IList<char> polymer, IList<Rule> rules, int steps)
+    {
+        var pairCounts = new Dictionary<(char, char), long>();
+        for (var i = 0; i < polymer.Count - 1; i++)
+        {
+            var key = (polymer[i], polymer[i + 1]);
+            pairCounts.EnsureContainsKey(key);
+            pairCounts[key]++;
+        }
+
+        for (var i = 0; i < steps; i++)
+        {
+            ExtendPolymer(pairCounts, rules);
+        }
+
+        return pairCounts;
+    }
+
+    private static void ExtendPolymer(Dictionary<(char, char), long> pairCounts, IList<Rule> rules)
+    {
+        var deltas = new Dictionary<(char, char), long>();
+        foreach (var (lhs, rhs) in pairCounts.Keys.Freeze())
+        {
+            foreach (var rule in rules.Where(rule => rule.Lhs == lhs && rule.Rhs == rhs))
+            {
+                var lPairKey = (lhs, rule.Insert);
+                var rPairKey = (rule.Insert, rhs);
+
+                deltas.EnsureContainsKey(rule.MatchKey);
+                deltas.EnsureContainsKey(lPairKey);
+                deltas.EnsureContainsKey(rPairKey);
+
+                deltas[rule.MatchKey] -= pairCounts[rule.MatchKey];
+                deltas[lPairKey] += pairCounts[rule.MatchKey];
+                deltas[rPairKey] += pairCounts[rule.MatchKey];
+            }
+        }
+
+        foreach (var (pairKey, delta) in deltas)
+        {
+            pairCounts.EnsureContainsKey(pairKey);
+            pairCounts[pairKey] += delta;
+        }
+    }
+
+    private static void Parse(IList<string> input, out List<char> polymer, out List<Rule> rules)
+    {
+        polymer = new List<char>(input[0]);
+        rules = input.Skip(2).Select(ParseRule).ToList();
+    }
+
+    private static Rule ParseRule(string line)
+    {
+        var elements = Regex.Match(line, RuleRegex);
+        return new Rule
+        {
+            Lhs = elements.Groups[1].Value[0],
+            Rhs = elements.Groups[2].Value[0],
+            Insert = elements.Groups[3].Value[0],
+        };
+    }
+}
--- SolutionRunner/Program.cs
+++ SolutionRunner/Program.cs
@@ -2,4 +2,4 @@
 
 using SolutionRunner;
 
-RunSolution.Do(2022,25);
+RunSolution.Do(2021,14);
