8c02b5833ca0d8fb133179d3c15e768369b6df10
trevor <tmbarker9@gmail.com>
2023-02-05 13:33:22 -0800

    Add 2019 day 17

---
+++ Problems/Y2019/D17/RoutineBuilder.cs
@@ -0,0 +1,68 @@
+using Problems.Common;
+
+namespace Problems.Y2019.D17;
+
+public static class RoutineBuilder
+{
+    private const int MinFuncLength = 2;
+    private const int MaxFuncLength = 20;
+
+    private const string VideoFeedOn = "y";
+    private const string VideoFeedOff = "n";
+    
+    public static IEnumerable<string> Build(IEnumerable<string> commands, bool videoFeedOn)
+    {
+        var stream = string.Join(',', commands) + ',';
+        var functions = ResolveFunctions(stream);
+
+        var routine = stream;
+        foreach (var (token, function) in functions)
+        {
+            routine = routine.Replace(function, token + ",");
+        }
+
+        var formatted = new List<string> { FormatLine(routine) };
+        formatted.AddRange(functions.Values.Select(FormatLine));
+        formatted.Add(FormatLine(videoFeedOn ? VideoFeedOn : VideoFeedOff));
+
+        return formatted;
+    }
+
+    private static Dictionary<char, string> ResolveFunctions(string commandStream)
+    {
+        for (var a = MinFuncLength; a <= MaxFuncLength && a < commandStream.Length; a++)
+        {
+            var aFunc = commandStream[..a];
+            var aRemaining = commandStream.Replace(aFunc, string.Empty);
+            
+            for (var b = MinFuncLength; b <= MaxFuncLength && b < aRemaining.Length; b++)
+            {
+                var bFunc = aRemaining[..b];
+                var bRemaining = aRemaining.Replace(bFunc, string.Empty);
+
+                for (var c = MinFuncLength; c <= MaxFuncLength && c < bRemaining.Length; c++)
+                {
+                    var cFunc = bRemaining[..c];
+                    var cRemaining = bRemaining.Replace(cFunc, string.Empty);
+                    
+                    if (string.IsNullOrWhiteSpace(cRemaining))
+                    {
+                        return new Dictionary<char, string>
+                        {
+                            {'A', aFunc},
+                            {'B', bFunc},
+                            {'C', cFunc},
+                        };
+                    }
+                }
+            }
+        }
+
+        throw new NoSolutionException();
+    }
+
+    private static string FormatLine(string raw)
+    {
+        return raw.TrimEnd(',') + '\n';
+    }
+}
---
+++ Problems/Y2019/D17/Solution.cs
@@ -0,0 +1,156 @@
+using System.Text;
+using Problems.Common;
+using Problems.Y2019.Common;
+using Problems.Y2019.IntCode;
+using Utilities.Cartesian;
+
+namespace Problems.Y2019.D17;
+
+/// <summary>
+/// Set and Forget: https://adventofcode.com/2019/day/17
+/// </summary>
+public class Solution : SolutionBase2019
+{
+    private const char Scaffold = '#';
+    
+    private static readonly Dictionary<char, Vector2D> Directions = new()
+    {
+        // NOTE: The UV coordinate system is used for this problem, while the Vector2D static vectors are XY
+        { 'v', Vector2D.Up },
+        { '^', Vector2D.Down },
+        { '<', Vector2D.Left },
+        { '>', Vector2D.Right },
+    };
+    private static readonly Dictionary<char, Rotation3D> TurnCommands = new()
+    {
+        { 'L', Rotation3D.Negative90Z },
+        { 'R', Rotation3D.Positive90Z }
+    };
+
+    public override int Day => 17;
+    
+    public override object Run(int part)
+    {
+        var ascii = GetCameraOutput();
+        var state = ParseCameraOutput(ascii);
+        
+        return part switch
+        {
+            0 => SumAlignmentParams(state.Scaffolding),
+            1 => GetCollectedDust(state.Scaffolding, state.Robot),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private static int SumAlignmentParams(IReadOnlySet<Vector2D> positions)
+    {
+        return positions
+            .Where(p => p.GetAdjacentSet(DistanceMetric.Taxicab).Count(positions.Contains) == 4)
+            .Aggregate(0, (sum, pos) => sum + pos.X * pos.Y);
+    }
+
+    private long GetCollectedDust(IReadOnlySet<Vector2D> positions, Pose pose)
+    {
+        var robot = IntCodeVm.Create(LoadRobotProgram());
+        var commands = ComputeCommands(positions, pose);
+
+        foreach (var line in RoutineBuilder.Build(commands, false))
+        {
+            foreach (var chr in line)
+            {
+                robot.InputBuffer.Enqueue(chr);
+            }
+        }
+        
+        return robot.Run() == IntCodeVm.ExitCode.Halted
+            ? robot.OutputBuffer.Last()
+            : throw new NoSolutionException();
+    }
+
+    private static IList<string> ComputeCommands(IReadOnlySet<Vector2D> positions, Pose pose)
+    {
+        var commands = new List<string>();
+        var visited = new HashSet<Vector2D> { pose.Position };
+
+        while (visited.Count < positions.Count)
+        {
+            foreach (var (cmd, turn) in TurnCommands)
+            {
+                if (!positions.Contains(pose.Turn(turn).Step().Position))
+                {
+                    continue;
+                }
+                
+                pose = pose.Turn(turn);
+                commands.Add(cmd.ToString());
+                break;
+            }
+
+            var steps = 0;
+            while (positions.Contains(pose.Step().Position))
+            {
+                steps++;
+                pose = pose.Step();
+                visited.Add(pose.Position);
+            }
+            
+            commands.Add(steps.ToString());
+        }
+
+        return commands;
+    }
+
+    private static State ParseCameraOutput(string ascii)
+    {
+        var scaffolding = new HashSet<Vector2D>();
+        var robotPose = new Pose(Vector2D.Zero, Vector2D.Zero);
+
+        var rows = ascii.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var cols = rows[0].Length;
+
+        for (var y = 0; y < rows.Length; y++)
+        for (var x = 0; x < cols; x++)
+        {
+            var chr = rows[y][x];
+            var pos = new Vector2D(x, y);
+
+            if (Directions.ContainsKey(chr))
+            {
+                scaffolding.Add(pos);
+                robotPose = new Pose(pos, Directions[chr]);
+            }
+            
+            if (chr == Scaffold)
+            {
+                scaffolding.Add(pos);
+            }
+        }
+
+        return new State(scaffolding, robotPose);
+    }
+
+    private string GetCameraOutput()
+    {
+        var vm = IntCodeVm.Create(LoadIntCodeProgram());
+        vm.Run();
+        return ReadAsciiOutput(vm);
+    }
+
+    private static string ReadAsciiOutput(IntCodeVm vm)
+    {
+        var sb = new StringBuilder();
+        while (vm.OutputBuffer.Any())
+        {
+            sb.Append((char)vm.OutputBuffer.Dequeue());
+        }
+
+        return sb.ToString();
+    }
+    
+    private IList<long> LoadRobotProgram()
+    {
+        var unmodified = LoadIntCodeProgram();
+        unmodified[0] = 2L;
+        return unmodified;
+    }
+}
