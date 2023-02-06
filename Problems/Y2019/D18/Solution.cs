a88919707b5c69da1d4f6fc61df4480d0f2193f1
tmbarker <tmbarker9@gmail.com>
2023-02-05 17:18:16 -0800

    Add 2019-18 template

---
+++ Problems/Y2019/D18/Solution.cs
@@ -0,0 +1,22 @@
+﻿using Problems.Y2019.Common;
+
+namespace Problems.Y2019.D18;
+
+/// <summary>
+/// Many-Worlds Interpretation: https://adventofcode.com/2019/day/18
+/// </summary>
+public class Solution : SolutionBase2019
+{
+    private const char Start = '@';
+    private const char Wall = '#';
+
+    public override int Day => 18;
+    
+    public override object Run(int part)
+    {
+        return part switch
+        {
+            _ => ProblemNotSolvedString
+        };
+    }
+}
--- SolutionRunner/Program.cs
+++ SolutionRunner/Program.cs
@@ -2,4 +2,4 @@
 
 using SolutionRunner;
 
-RunSolution.Do(2019, 17);
+RunSolution.Do(2019, 18);
