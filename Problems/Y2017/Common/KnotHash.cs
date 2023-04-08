f30f136979f8a35746c223e70eeb2e9a1b8950ac
trevor <tmbarker9@gmail.com>
2023-04-08 14:34:21 -0700

    Move KnotHash to a more common namespace

---
+++ Problems/Y2017/Common/KnotHash.cs
@@ -0,0 +1,67 @@
+namespace Problems.Y2017.Common;
+
+public static class KnotHash
+{
+    private const int SparseBytes = 256;
+    private const int DenseBytes = 16;
+    private const int Rounds = 64;
+
+    private static readonly byte[] LengthSuffixValues = { 17, 31, 73, 47, 23 };
+    
+    public static string ComputeHash(string input)
+    {
+        var dense = new byte[DenseBytes];
+        var sparse = Enumerable.Range(0, SparseBytes)
+            .Select(n => (byte)n)
+            .ToArray();
+        var ascii = input
+            .Select(c => (byte)c)
+            .Concat(LengthSuffixValues)
+            .ToArray();
+        
+        sparse = TieKnots(
+            buffer: sparse,
+            lengths: ascii,
+            rounds: Rounds);
+        
+        for (var i = 0; i < dense.Length; i++)
+        for (var j = 0; j < dense.Length; j++)
+        {
+            dense[i] ^= sparse[dense.Length * i + j];
+        }
+
+        return Convert.ToHexString(dense).ToLower();
+    }
+    
+    public static byte[] TieKnots(byte[] buffer, byte[] lengths, int rounds)
+    {
+        var currentPos = 0;
+        var skipLength = 0;
+
+        for (var i = 0; i < rounds; i++)
+        {
+            foreach (var length in lengths)
+            {
+                ReverseSegment(buffer, start: currentPos, count: length);
+                currentPos = (currentPos + length + skipLength) % buffer.Length;
+                skipLength++;
+            }   
+        }
+
+        return buffer;
+    }
+
+    private static void ReverseSegment(IList<byte> buffer, int start, int count)
+    {
+        var stack = new Stack<byte>();
+        for (var i = 0; i < count; i++)
+        {
+            stack.Push(buffer[(start + i) % buffer.Count]);
+        }
+
+        for (var i = 0; i < count; i++)
+        {
+            buffer[(start + i) % buffer.Count] = stack.Pop();
+        }
+    }
+}
--- Problems/Y2017/D10/Solution.cs
+++ Problems/Y2017/D10/Solution.cs
@@ -8,87 +8,35 @@ namespace Problems.Y2017.D10;
 /// </summary>
 public class Solution : SolutionBase2017
 {
-    private static readonly byte[] LengthSuffixValues = { 17, 31, 73, 47, 23 };
-    
     public override int Day => 10;
     
     public override object Run(int part)
     {
         var input = GetInputText();
-        var circle = Enumerable.Range(0, 256)
-            .Select(n => (byte)n)
-            .ToArray();
-        
         return part switch
         {
-            1 => HashSimple(input, circle),
-            2 => HashFull(input, circle),
+            1 => HashSimple(input),
+            2 => HashFull(input),
             _ => ProblemNotSolvedString
         };
     }
 
-    private static int HashSimple(string input, byte[] circle)
+    private static int HashSimple(string input)
     {
-        var lengths = input.ParseInts().Select(n => (byte)n).ToArray();
-        var sparse = Hash(
-            circle: circle,
-            lengths: lengths,
-            rounds: 1);
-        
-        return sparse[0] * sparse[1];
+        var lengths = GetByteArray(input.ParseInts());
+        var ring = GetByteArray(Enumerable.Range(0, 256));
+        var result = KnotHash.TieKnots(ring, lengths, rounds: 1);
+
+        return result[0] * result[1];
     }
 
-    private static string HashFull(string input, byte[] circle)
+    private static string HashFull(string input)
     {
-        var ascii = input
-            .Select(c => (byte)c)
-            .Concat(LengthSuffixValues)
-            .ToArray();
-        
-        var sparse = Hash(
-            circle: circle,
-            lengths: ascii,
-            rounds: 64);
-        
-        var dense = new byte[16];
-        for (var i = 0; i < 16; i++)
-        for (var j = 0; j < 16; j++)
-        {
-            dense[i] ^= sparse[16 * i + j];
-        }
-
-        return Convert.ToHexString(dense).ToLower();
-    }
-    
-    private static byte[] Hash(byte[] circle, byte[] lengths, int rounds)
-    {
-        var currentPos = 0;
-        var skipLength = 0;
-
-        for (var i = 0; i < rounds; i++)
-        {
-            foreach (var length in lengths)
-            {
-                ReverseSegment(circle, currentPos, length);
-                currentPos = (currentPos + length + skipLength) % circle.Length;
-                skipLength++;
-            }   
-        }
-
-        return circle;
+        return KnotHash.ComputeHash(input);
     }
 
-    private static void ReverseSegment(IList<byte> circle, int start, int count)
+    private static byte[] GetByteArray(IEnumerable<int> ints)
     {
-        var stack = new Stack<byte>();
-        for (var i = 0; i < count; i++)
-        {
-            stack.Push(circle[(start + i) % circle.Count]);
-        }
-
-        for (var i = 0; i < count; i++)
-        {
-            circle[(start + i) % circle.Count] = stack.Pop();
-        }
+        return ints.Select(n => (byte)n).ToArray();
     }
 }
