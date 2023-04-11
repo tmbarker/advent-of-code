e96b1732e1f4844f2ad6a57776304b36357bb6f3
trevor <tmbarker9@gmail.com>
2023-04-10 22:16:19 -0700

    Add 2017-19

---
+++ Problems/Y2017/D19/Solution.cs
@@ -0,0 +1,70 @@
+using System.Text;
+using Problems.Y2017.Common;
+using Utilities.Cartesian;
+
+namespace Problems.Y2017.D19;
+
+/// <summary>
+/// A Series of Tubes: https://adventofcode.com/2017/day/19
+/// </summary>
+public class Solution : SolutionBase2017
+{
+    private const char Junction = '+';
+    private static readonly HashSet<char> Traversable = new() { '|', '-', '+' };
+
+    public override int Day => 19;
+    
+    public override object Run(int part)
+    {
+        var input = GetInputLines();
+        var map = Grid2D<char>.MapChars(input, c => c);
+        var start = new Vector2D(
+            x: input[0].IndexOf('|'),
+            y: map.Height - 1);
+
+        return part switch
+        {
+            1 => Traverse(map, start).Letters,
+            2 => Traverse(map, start).Steps,
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private static PathSummary Traverse(Grid2D<char> map, Vector2D start)
+    {
+        var letters = new StringBuilder();
+        var steps = 1;
+        
+        var face = Vector2D.Down;
+        var pos = start;
+
+        while (CanMoveTo(map, pos: pos + face))
+        {
+            pos += face;
+            steps++;
+            
+            if (char.IsLetter(map[pos]))
+            {
+                letters.Append(map[pos]);
+            }
+
+            if (map[pos] == Junction)
+            {
+                face = CanMoveTo(map, pos + (Vector2D)(Rotation3D.Positive90Z * face))
+                    ? Rotation3D.Positive90Z * face
+                    : Rotation3D.Negative90Z * face;
+            }
+        }
+
+        return new PathSummary(
+            Letters: letters.ToString(),
+            Steps: steps);
+    }
+
+    private static bool CanMoveTo(Grid2D<char> map, Vector2D pos)
+    {
+        return map.IsInDomain(pos) && (Traversable.Contains(map[pos]) || char.IsLetter(map[pos]));
+    }
+
+    private readonly record struct PathSummary(string Letters, int Steps);
+}
