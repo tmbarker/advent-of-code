8f4897f53e3026221edfe174a782b8d4fa46c7b4
trevor <tmbarker9@gmail.com>
2023-04-12 21:06:51 -0700

    Add 2017-25

---
+++ Problems/Y2017/D25/Solution.cs
@@ -0,0 +1,56 @@
+using Problems.Y2017.Common;
+using Utilities.Extensions;
+
+namespace Problems.Y2017.D25;
+
+/// <summary>
+/// The Halting Problem: https://adventofcode.com/2017/day/25
+/// </summary>
+public class Solution : SolutionBase2017
+{
+    public override int Day => 25;
+    public override int Parts => 1;
+
+    public override object Run(int part)
+    {
+        return part switch
+        {
+            1 => Simulate(),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private int Simulate()
+    {
+        ParseInput(input: GetInputLines(), out var state, out var rules, out var steps);
+
+        var machine = new TuringMachine(rules);
+        var count = machine.Run(state, steps);
+
+        return count;
+    }
+    
+    private static void ParseInput(IReadOnlyList<string> input, 
+        out char state, out Dictionary<char, TuringMachine.State> rules, out int steps)
+
+    {
+        state = input[0][^2];
+        rules = new Dictionary<char, TuringMachine.State>();
+        steps = input[1].Split(' ')[^2].ParseInt();
+
+        foreach (var chunk in input.Skip(3).Chunk(10))
+        {
+            var id = chunk[0][^2];
+            var falseTransition = new TuringMachine.Transition(
+                Write: chunk[2][^2] == '1',
+                Move: chunk[3].Contains("right") ? 1 : -1,
+                Next: chunk[4][^2]);
+            var trueTransition = new TuringMachine.Transition(
+                Write: chunk[6][^2] == '1',
+                Move: chunk[7].Contains("right") ? 1 : -1,
+                Next: chunk[8][^2]);
+
+            rules[id] = new TuringMachine.State(falseTransition, trueTransition);
+        }
+    }
+}
---
+++ Problems/Y2017/D25/TuringMachine.cs
@@ -0,0 +1,37 @@
+namespace Problems.Y2017.D25;
+
+public class TuringMachine
+{
+    private readonly Dictionary<char, State> _ruleTable;
+    private readonly Dictionary<int, bool> _tape = new();
+    private int _cursor;
+    
+    public TuringMachine(Dictionary<char, State> ruleTable)
+    {
+        _ruleTable = ruleTable;
+    }
+
+    public int Run(char state, int steps)
+    {
+        _tape.Clear();
+        _cursor = 0;
+        
+        for (var i = 0; i < steps; i++)
+        {
+            var rule = _ruleTable[state];
+            var transition = _tape.TryGetValue(_cursor, out var value) && value
+                ? rule.True
+                : rule.False;
+
+            _tape[_cursor] = transition.Write;
+            _cursor += transition.Move;
+            
+            state = transition.Next;
+        }
+
+        return _tape.Values.Count(b => b);
+    }
+    
+    public readonly record struct Transition(bool Write, int Move, char Next);
+    public readonly record struct State(Transition False, Transition True);
+}
