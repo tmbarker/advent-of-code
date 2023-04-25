1b3f7f926901305e1198297cd8af85a45a295389
trevor <tmbarker9@gmail.com>
2023-04-25 08:09:18 -0700

    Add 2016-13

---
+++ Problems/Y2016/D13/Solution.cs
@@ -0,0 +1,119 @@
+using Problems.Common;
+using Utilities.Cartesian;
+
+namespace Problems.Y2016.D13;
+
+/// <summary>
+/// A Maze of Twisty Little Cubicles: https://adventofcode.com/2016/day/13
+/// </summary>
+public class Solution : SolutionBase
+{
+    private static readonly Vector2D Start = new (x: 1, y: 1);
+    private static readonly Vector2D Target = new (x: 31, y: 39);
+    
+    public override object Run(int part)
+    {
+        var input = GetInputText();
+        var favourite = int.Parse(input);
+
+        return part switch
+        {
+            1 => Search(Start, Target, favourite),
+            2 => Explore(Start, favourite, maxDepth: 50),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private static int Search(Vector2D start, Vector2D target, int favourite)
+    {
+        var queue = new Queue<Vector2D>(new[] { start });
+        var visited = new HashSet<Vector2D>(new[] { start });
+        var depth = 0;
+
+        while (queue.Any())
+        {
+            var nodesAtDepth = queue.Count;
+            while (nodesAtDepth-- > 0)
+            {
+                var pos = queue.Dequeue();
+                if (pos == target)
+                {
+                    return depth;
+                }
+
+                var adjacent = GetAdjacentOpen(pos, favourite);
+                var unvisited = adjacent.Where(adj => !visited.Contains(adj));
+
+                foreach (var adj in unvisited)
+                {
+                    visited.Add(adj);
+                    queue.Enqueue(adj);
+                }
+            }
+
+            depth++;
+        }
+
+        throw new NoSolutionException();
+    }
+    
+    private static int Explore(Vector2D start, int favourite, int maxDepth)
+    {
+        var queue = new Queue<Vector2D>(new[] { start });
+        var visited = new HashSet<Vector2D>(new[] { start });
+        var depth = 0;
+
+        while (queue.Any() && depth < maxDepth)
+        {
+            var nodesAtDepth = queue.Count;
+            while (nodesAtDepth-- > 0)
+            {
+                var pos = queue.Dequeue();
+                var adjacent = GetAdjacentOpen(pos, favourite);
+                var unvisited = adjacent.Where(adj => !visited.Contains(adj));
+
+                foreach (var adj in unvisited)
+                {
+                    visited.Add(adj);
+                    queue.Enqueue(adj);
+                }
+            }
+
+            depth++;
+        }
+
+        return visited.Count;
+    }
+
+    private static IEnumerable<Vector2D> GetAdjacentOpen(Vector2D pos, int favourite)
+    {
+        return pos
+            .GetAdjacentSet(Metric.Taxicab)
+            .Where(adj => adj is { X: >= 0, Y: >= 0 })
+            .Where(adj => IsOpen(adj, favourite));
+    }
+
+    private static bool IsOpen(Vector2D pos, int favourite)
+    {
+        var raw = pos.X * pos.X + 3 * pos.X + 2 * pos.X * pos.Y + pos.Y + pos.Y * pos.Y;
+        var sum = raw + favourite;
+        var set = CountSetBits(sum);
+
+        return set % 2 == 0;
+    }
+
+    private static int CountSetBits(int n)
+    {
+        var set = 0;
+        while (n > 0)
+        {
+            if (n % 2 > 0)
+            {
+                set++;
+            }
+            n /= 2;
+        }
+
+        return set;
+    }
+}
