2852830977589be20e0a95e9058defe74f1cf988
trevor <tmbarker9@gmail.com>
2023-04-25 22:30:55 -0700

    Add 2016-17, update favourites and README

---
+++ Problems/Y2016/D17/PathCriteria.cs
@@ -0,0 +1,7 @@
+namespace Problems.Y2016.D17;
+
+public enum PathCriteria
+{
+    Shortest,
+    Longest
+}
---
+++ Problems/Y2016/D17/State.cs
@@ -0,0 +1,5 @@
+using Utilities.Cartesian;
+
+namespace Problems.Y2016.D17;
+
+public readonly record struct State(Vector2D Pos, string Path);
