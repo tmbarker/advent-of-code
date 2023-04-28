dac7ed21942ee741ff84091a6c8f5147ecaa8107
trevor <tmbarker9@gmail.com>
2023-04-28 13:11:14 -0700

    2020-04: Clean up regexes

--- Problems/Y2020/D04/Solution.cs
+++ Problems/Y2020/D04/Solution.cs
@@ -13,32 +13,34 @@ public class Solution : SolutionBase
     private const RegexOptions Options = RegexOptions.Multiline;
     private static readonly IReadOnlyDictionary<string, string> FieldValidators = new Dictionary<string, string>
     {
-        {"byr", @"^(?=.*byr:((19[2-9][0-9])|(200[0-2]))\b).*"},
-        {"iyr", @"^(?=.*iyr:20((1[0-9])|20)\b).*"},
-        {"eyr", @"^(?=.*eyr:20((2[0-9])|30)\b).*"},
-        {"hgt", @"^(?=.*hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)\b).*"},
-        {"hcl", @"^(?=.*hcl:#[0-9a-f]{6}\b).*"},
-        {"ecl", @"^(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)\b).*"},
-        {"pid", @"^(?=.*pid:\d{9}\b).*"},
+        {"byr", @"(?=.*byr:((19[2-9][0-9])|(200[0-2]))\b)"},
+        {"iyr", @"(?=.*iyr:20((1[0-9])|20)\b)"},
+        {"eyr", @"(?=.*eyr:20((2[0-9])|30)\b)"},
+        {"hgt", @"(?=.*hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)\b)"},
+        {"hcl", @"(?=.*hcl:#[0-9a-f]{6}\b)"},
+        {"ecl", @"(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)\b)"},
+        {"pid", @"(?=.*pid:\d{9}\b)"}
     };
 
     public override object Run(int part)
     {
-        var passports = ParsePassportData(GetInputText());
+        var input = GetInputText();
+        var passports = ParsePassportData(input);
+        
         return part switch
         {
-            1 => passports.Count(ValidatePassportFieldPresence),
-            2 => passports.Count(ValidatePassportFieldData),
+            1 => passports.Count(ValidateFieldPresence),
+            2 => passports.Count(ValidateFieldData),
             _ => ProblemNotSolvedString
         };
     }
 
-    private static bool ValidatePassportFieldPresence(string passport)
+    private static bool ValidateFieldPresence(string passport)
     {
         return FieldValidators.Keys.All(passport.Contains);
     }
     
-    private static bool ValidatePassportFieldData(string passport)
+    private static bool ValidateFieldData(string passport)
     {
         return FieldValidators.Values.All(regex => Regex.IsMatch(passport, regex, Options));
     }
