9972b54721d70028d5ddc0935823559ea931afe0
trevor <tmbarker9@gmail.com>
2023-05-03 07:13:33 -0700

    Add 2015-23

---
+++ Problems/Y2015/D23/Solution.cs
@@ -0,0 +1,28 @@
+using Problems.Common;
+
+namespace Problems.Y2015.D23;
+
+/// <summary>
+/// Opening the Turing Lock: https://adventofcode.com/2015/day/23
+/// </summary>
+public class Solution : SolutionBase
+{
+    public override object Run(int part)
+    {
+        return part switch
+        {
+            1 => Emulate(a: 0L),
+            2 => Emulate(a: 1L),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private long Emulate(long a)
+    {
+        var program = GetInputLines();
+        var vm = new Vm { ['a'] = a };
+
+        vm.Run(program);
+        return vm['b'];
+    }
+}
