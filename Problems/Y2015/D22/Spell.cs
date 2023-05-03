76e15dc02a77b0898970eed10f198c37ad206f83
trevor <tmbarker9@gmail.com>
2023-05-03 06:48:14 -0700

    Add 2015-22

---
+++ Problems/Y2015/D22/Spell.cs
@@ -0,0 +1,10 @@
+namespace Problems.Y2015.D22;
+
+public enum Spell
+{
+    MagicMissile,
+    Drain,
+    Shield,
+    Poison,
+    Recharge
+}
---
+++ Problems/Y2015/D22/Units.cs
@@ -0,0 +1,4 @@
+namespace Problems.Y2015.D22;
+
+public readonly record struct Boss(int Hp, int Dmg);
+public readonly record struct Wizard(int Hp, int Mana, int Armor);
