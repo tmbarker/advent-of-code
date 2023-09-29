7249000a1ef991c1e21aaa51c77674f76ac66f6e
trevor <tmbarker9@gmail.com>
2023-09-28 22:05:20 -0700

    Re-solve 2020 19 using CYK

--- Problems/Y2020/D19/Solution.cs
+++ Problems/Y2020/D19/Solution.cs
@@ -1,127 +1,70 @@
-using System.Text;
-using System.Text.RegularExpressions;
 using Problems.Attributes;
 using Problems.Common;
+using Utilities.Language.ContextFree;
 
 namespace Problems.Y2020.D19;
-
-using Messages = IEnumerable<string>;
-using Rules = IDictionary<int, string>;
-using Memo = IDictionary<int, string>;
 
 /// <summary>
 /// Monster Messages: https://adventofcode.com/2020/day/19
 /// </summary>
-[Favourite("Monster Messages", Topics.RegularExpressions, Difficulty.Hard)]
+[Favourite("Monster Messages", Topics.FormalLanguage, Difficulty.Hard)]
 public class Solution : SolutionBase
 {
-    private const int Rule = 0;
+    private const string StartSymbol = "0";
+    private const StringSplitOptions Options = StringSplitOptions.TrimEntries;
 
-    /*
-     For part 2 we are given two rules, 8 and 11, which are recursive. We can handle both using custom Regexes as follows:
-     
-     Rule 8)  "42 | 42 8" Means that we want to have one or more instances of pattern 42, which is the
-              definition of the + token. Therefore, we can implement 8') using:
-              "42 +"
-     
-     Rule 11) "42 31 | 42 11 31" Means that we want to have an equal number of 42s and 31s (and at least one of each).
-              .NET Regex gives us Balancing Groups, which allows us to 'pop' the values captured to a named capture
-              group. The pattern will fail if we try to pop an empty group. Therefore we can create a named capture 
-              group which is pushed to as we match 42s, and popped as we match 31s. 
-              
-              The last thing we need to do is validate that our "stack" is empty after the final 31. To do so we
-              can use Conditional Patterns, (?(CONDITION)TRUE|FALSE). We are allowed to use capture group names for the
-              condition, which will fail if the group is non-empty. When only one condition is specified, that is the
-              fail pattern, so we can simply specify an empty negative lookahead (?!) to ensure that our match fails.
-              
-              Therefore, we can implement 11') using:
-              "(?<Stack> 42 )+(?<-Stack> 31 )+(?(Stack)(?!))"
-                    ^               ^              ^
-                   Push            Pop        Assert Empty
-    */ 
-    private static readonly Rules RuleOverrides = new Dictionary<int, string>
+    private static readonly Dictionary<string, string> Overrides = new()
     {
-        { 8,  "42 +" },
-        { 11, "(?<Stack> 42 )+(?<-Stack> 31 )+(?(Stack)(?!))" }
+        { "8", "42 | 42 8" },
+        { "11", "42 31 | 42 11 31" }
     };
 
     public override object Run(int part)
     {
-        ParseInput(GetInputLines(), out var rules, out var messages);
         return part switch
         {
-            1 => CountMatches(Rule, messages, rules, useRuleOverrides: false),
-            2 => CountMatches(Rule, messages, rules, useRuleOverrides: true),
+            1 => CountRecognitions(useOverrides: false),
+            2 => CountRecognitions(useOverrides: true),
             _ => ProblemNotSolvedString
         };
     }
 
-    private static int CountMatches(int ruleId, Messages messages, Rules rules, bool useRuleOverrides)
+    private int CountRecognitions(bool useOverrides)
     {
-        if (useRuleOverrides)
-        {
-            foreach (var (id, rule) in RuleOverrides)
-            {
-                rules[id] = rule;
-            }
-        }
+        var input = GetInputLines();
+        var sentences = input
+            .SkipWhile(line => string.IsNullOrWhiteSpace(line) || !char.IsLetter(line[0]))
+            .Select(ParseSentence);
+        var productions = input
+            .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+            .SelectMany(line => ParseRule(line, useOverrides));
+
+        var cfgGrammar = new Grammar(StartSymbol, productions);
+        var cnfGrammar = CnfConverter.Convert(cfgGrammar);
         
-        var memo = new Dictionary<int, string>();
-        var regex = new Regex($"^{BuildRegex(ruleId, rules, memo)}$");
-        
-        return messages.Count(m => regex.Match(m).Success);
+        return sentences.Count(s => CykParser.Recognize(
+            grammar: cnfGrammar,
+            sentence: s));
     }
-    
-    private static string BuildRegex(int ruleId, Rules rules, Memo memo)
+
+    private static IEnumerable<Production> ParseRule(string ruleStr, bool useOverrides)
     {
-        if (memo.TryGetValue(ruleId, out var builtRegex))
-        {
-            return builtRegex;
-        }
-        
-        var rule = rules[ruleId];
-        if (rule.Contains('"'))
-        {
-            memo[ruleId] = rule[rule.IndexOf('"') + 1].ToString();
-            return memo[ruleId];
-        }
-        
-        var hasAlternations = rule.Contains('|');
-        var tokens = rule.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        var regex = new StringBuilder();
+        var elements = ruleStr.Split(separator: ':', options: Options);
+        var nonTerminal = elements[0];
+        var yieldStr = useOverrides && Overrides.TryGetValue(nonTerminal, out var @override)
+            ? @override
+            : elements[1];
 
-        if (hasAlternations)
+        foreach (var alternation in yieldStr.Split(separator:'|', options: Options))
         {
-            regex.Append("(?:");
+            yield return new Production(
+                nonTerminal: nonTerminal,
+                yields: alternation.Trim('"').Split(separator: ' ', options: Options));
         }
-        
-        foreach (var token in tokens)
-        {
-            regex.Append(int.TryParse(token, out var nestedRuleId)
-                ? BuildRegex(nestedRuleId, rules, memo)
-                : token);
-        }
-
-        if (hasAlternations)
-        {
-            regex.Append(')');
-        }
-
-        memo[ruleId] = regex.ToString();
-        return memo[ruleId];
     }
-    
-    private static void ParseInput(IList<string> input, out Rules rules, out Messages messages)
-    {
-        rules = new Dictionary<int, string>();
-        for (var i = 0; !string.IsNullOrWhiteSpace(input[i]); i++)
-        {
-            var elements = input[i].Split(separator: ':');
-            var ruleId = int.Parse(elements[0]);
-            
-            rules.Add(ruleId, elements[1]);
-        }
 
-        messages = new List<string>(input.Skip(rules.Count + 1));
+    private static List<string> ParseSentence(string sentenceStr)
+    {
+        return sentenceStr.Select(c => c.ToString()).ToList();
     }
 }
