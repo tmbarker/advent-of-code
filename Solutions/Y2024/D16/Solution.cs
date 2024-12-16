ca1b40b8ccbc8ac96dd49f8815c99a9e57750d78
Trevor Barker <tmbarker9@gmail.com>
2024-12-15 21:40:58 -0800

    Solve Y2024 D16

---
+++ Solutions/Y2024/D16/Solution.cs
@@ -0,0 +1,82 @@
+using System.Collections.Immutable;
+using Utilities.Collections;
+using Utilities.Geometry.Euclidean;
+
+namespace Solutions.Y2024.D16;
+
+using Path = ImmutableStack<Vec2D>;
+
+[PuzzleInfo("Reindeer Maze", Topics.Vectors|Topics.Graphs, Difficulty.Easy)]
+public class Solution : SolutionBase
+{
+    private readonly record struct Result(int MinScore, List<Path> BestPaths);
+    private readonly record struct State(Pose2D Pose, int Score, Path Path)
+    {
+        public State Step()  => new(Pose.Step(), Score + 1, Path.Push(Pose.Ahead));
+        public State Left()  => new(Pose.Turn(Rot3D.P90Z), Score + 1000, Path);
+        public State Right() => new(Pose.Turn(Rot3D.N90Z), Score + 1000, Path);
+    }
+    
+    public override object Run(int part)
+    {
+        var lines = GetInputLines();
+        var grid = Grid2D<char>.MapChars(lines);
+        var result = Navigate(grid);
+
+        return part == 1
+            ? result.MinScore
+            : result.BestPaths
+                .SelectMany(path => path)
+                .Distinct()
+                .Count();
+    }
+    
+    private static Result Navigate(Grid2D<char> grid)
+    {
+        var pose = new Pose2D(Pos: grid.Single(pos => grid[pos] == 'S'), Face: Vec2D.Right);
+        var seed = new State(pose, Score: 0, Path: [pose.Pos]);
+        var queue = new Queue<State>([seed]);
+        
+        var bestScore = int.MaxValue;
+        var bestPaths = new List<Path>();
+        var minScores = new DefaultDict<Pose2D, int>(defaultValue: int.MaxValue);
+        
+        while (queue.Count != 0)
+        {
+            var state = queue.Dequeue();
+            if (state.Score > bestScore)
+            {
+                continue;
+            }
+            
+            if (grid[state.Pose.Pos] == 'E')
+            {
+                if (state.Score == bestScore)
+                {
+                    bestPaths.Add(state.Path);
+                }
+                else if (state.Score < bestScore)
+                {
+                    bestPaths = [state.Path];
+                    bestScore = state.Score;
+                }
+                continue;
+            }
+
+            if (grid[state.Pose.Ahead] != '#') EnqueueIfBetter(state.Step());
+            if (grid[state.Pose.Left]  != '#') EnqueueIfBetter(state.Left());
+            if (grid[state.Pose.Right] != '#') EnqueueIfBetter(state.Right());
+        }
+
+        return new Result(bestScore, bestPaths);
+        
+        void EnqueueIfBetter(State candidate)
+        {
+            if (minScores[candidate.Pose] >= candidate.Score)
+            {
+                minScores[candidate.Pose] = candidate.Score;
+                queue.Enqueue(candidate);
+            }
+        }
+    }
+}
