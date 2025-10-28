627c55898d9f6689f986f71f31a4915d08881c30
Trevor Barker <tmbarker9@gmail.com>
2025-10-27 23:14:41 -0700

    Add Parent tracking to BinaryTreeNode<T>

--- Utilities/Graph/BinaryTreeNode.cs
+++ Utilities/Graph/BinaryTreeNode.cs
@@ -7,9 +7,51 @@ namespace Utilities.Graph;
 /// <typeparam name="T">The type associated with node value</typeparam>
 public sealed class BinaryTreeNode<T>(T value)
 {
+    private BinaryTreeNode<T>? _left;
+    private BinaryTreeNode<T>? _right;
+    
     public T Value { get; set; } = value;
-    public BinaryTreeNode<T>? Left { get; set; }
-    public BinaryTreeNode<T>? Right { get; set; }
+    public BinaryTreeNode<T>? Parent { get; private set; }
+    public BinaryTreeNode<T>? Left
+    {
+        get => _left;
+        set
+        {
+            if (_left == value)
+            {
+                return;
+            }
+            
+            ClearParent(value);
+            ClearParent(_left);
+            _left = value;
+            
+            if (_left != null)
+            {
+                _left.Parent = this;
+            }
+        }
+    }
+    public BinaryTreeNode<T>? Right
+    {
+        get => _right;
+        set
+        {
+            if (_right == value)
+            {
+                return;
+            }
+            
+            ClearParent(value);
+            ClearParent(_right);
+            _right = value;
+            
+            if (_right != null)
+            {
+                _right.Parent = this;
+            }
+        }
+    }
 
     /// <summary>
     ///     Print the <see cref="BinaryTreeNode{T}" /> and it's children to the console.
@@ -21,5 +63,17 @@ public sealed class BinaryTreeNode<T>(T value)
     public void Print(Func<T, string>? formatter = null, int spacing = 2, int topMargin = 1, int leftMargin = 1)
     {
         BinaryTreePrinter.Print(root: this, formatter, spacing, topMargin, leftMargin);
+    }
+
+    private static void ClearParent(BinaryTreeNode<T>? node)
+    {
+        if (node?.Parent == null)
+        {
+            return;
+        }
+        
+        if (node.Parent._left == node)  node.Parent._left = null;
+        if (node.Parent._right == node) node.Parent._right = null;
+        node.Parent = null;
     }
 }
